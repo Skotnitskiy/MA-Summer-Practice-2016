222c22f1c97ccec30b1697e6e1793070a8ffde7d
serh <sergeyskotnitskiy@gmail.com>
2016-07-21 23:41:16 +0300

    configured flask app

--- homeworks/team-3/tests/api/api.py
+++ homeworks/team-3/tests/api/api.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
 
 api.add_resource(HelloWorld, '/')
 
