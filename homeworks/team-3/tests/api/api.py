a0087c956b8416b458be91ad769234a60bddab03
serh <sergeyskotnitskiy@gmail.com>
2016-07-25 14:48:07 +0300

    created /questions  and /questions/id

--- homeworks/team-3/tests/api/api.py
+++ homeworks/team-3/tests/api/api.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask
+
+import sqlalchemy
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from testq import Test
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -8,12 +11,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class MainQuestions(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return Test.query.first().body['main-questions']
 
 
-api.add_resource(HelloWorld, '/')
+class MainQuestion(Resource):
+    def get(self, id_question):
+        return Test.query.first().body['main-questions'][id_question]
+
+
+api.add_resource(MainQuestions, '/questions')
+api.add_resource(MainQuestion, '/questions/<id_question>')
 
 if __name__ == '__main__':
     app.run()
