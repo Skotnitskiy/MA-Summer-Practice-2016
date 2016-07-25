99f3470a47553493749b2aba010792e7c42d9561
serh <sergeyskotnitskiy@gmail.com>
2016-07-25 19:29:33 +0300

    created post and update on MainQuestions

--- homeworks/team-3/tests/api/api.py
+++ homeworks/team-3/tests/api/api.py
@@ -1,9 +1,8 @@
 import os
-
-import sqlalchemy
-from flask import Flask, jsonify
+from flask import Flask, request, redirect
 from flask_restful import Resource, Api
 from testq import Test
+from database import db_session as dbs
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -11,9 +10,29 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 
+def update_create():
+    js_question = request.json
+    number = request.args.get('num')
+    body = Test.query.first().body
+    body['main-questions'].update({number: js_question})
+    test = Test.query.first()
+    test.body = body
+    dbs.add(test)
+    dbs.commit()
+    pass
+
+
 class MainQuestions(Resource):
     def get(self):
         return Test.query.first().body['main-questions']
+
+    def post(self):
+        update_create()
+        return redirect('/questions')
+
+    def put(self):
+        update_create()
+        pass
 
 
 class MainQuestion(Resource):
