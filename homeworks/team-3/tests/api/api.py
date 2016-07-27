f4be2b5af0103717fba1da313f5af0686adf7787
serh <sergeyskotnitskiy@gmail.com>
2016-07-27 12:59:33 +0300

    created route classes folder

--- homeworks/team-3/tests/api/api.py
+++ homeworks/team-3/tests/api/api.py
@@ -1,15 +1,12 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from questions import MainQuestions, MainQuestion
-
+from route_classes.questions import MainQuestions, MainQuestion
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
-
-
 
 api.add_resource(MainQuestions, '/questions')
 api.add_resource(MainQuestion, '/questions/<id_question>')
