8b832d3237079212a7357b580ab17635fbebf565
matvey <hodovanyuk.mi@gmail.com>
2016-07-29 12:11:03 +0300

    created Ajax method get to make async response to server and call callback function on success

---
+++ homeworks/team-3/tests/api/frontend/js/Ajax.js
@@ -0,0 +1,39 @@
+//ajax request methods
+var $ = {
+	xhr : new XMLHttpRequest();
+};
+$.get = function(url,cb){
+	var res = url+'res';
+	$.xhr.open('GET', encodeURI(url));
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+	    	cb(xhr.responseText);
+	    }
+	    else {
+	        alert('Request failed.  Returned status of ' + xhr.status);
+	    }
+	};
+	//return res;
+}
+$.post = function(url,data,cb){}
+$.put = function(url,data,cb){}
+$.delete = function(url,cb){}
+
+/*
+var xhr = new XMLHttpRequest();
+xhr.open('GET', encodeURI('https://jsonblob.com/api/jsonBlob/579a562be4b0dc55a4e88dbd'));
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        console.log(xhr.responseText);
+        alert('User\'s name is ' + xhr.responseText);
+    }
+    else {
+        alert('Request failed.  Returned status of ' + xhr.status);
+    }
+};
+xhr.send();
+
+xhr.open("POST", "https://jsonblob.com/api/jsonBlob/579a562be4b0dc55a4e88dbd", true); 
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");                  
+xhr.send(data);
+*/
