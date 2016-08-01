398744451c3dba125314ef9ed9bceea6d84ec38a
matvey <hodovanyuk.mi@gmail.com>
2016-08-02 01:08:09 +0300

    added style

--- homeworks/team-3/tests/api/static/js/Test.js
+++ homeworks/team-3/tests/api/static/js/Test.js
@@ -4,6 +4,7 @@ function Test(url, container) {
         this.questionID = 1;
         this.profession = '';
         this.professions = {};
+        this.madeChoise = false;
     };
     this.init = function () {
         $.get(this.url, this.setStore);
@@ -26,16 +27,20 @@ function Test(url, container) {
      *
      * @param e
      * */
-    this.onSubmit = function (e) {
-        this.questionID += 1;
+    this.onSubmit = function () {
+        if(this.madeChoise){
+            this.questionID += 1;
         if (this.professions[this.profession]) {
             this.professions[this.profession] += 1;
         } else {
             this.professions[this.profession] = 1;
         }
         this.render();
+            this.madeChoise = false;
+        }
     };
     this.onClick = function (t) {
+        this.madeChoise = true;
         this.profession = t.value;
     };
     this.renderTest = function () {
@@ -55,25 +60,39 @@ function Test(url, container) {
             }
         }
         return `
-			<div>
-				${currentState.body}
-				<form onsubmit="event.preventDefault(); onSubmit(event);">
-				    ${answers}
-				    <input type="submit" value="Submit">
-                </form>
-			</div>
+            <div class="header post-header">
+                 <div class="header-wrapper">
+                    <div class="wrapper wrapper-left">
+                    </div>
+                    <div class="header-middle">
+                        <div class="title">${currentState.body}</div>
+                    </div>
+                    <div class="wrapper wrapper-right-check">
+                        <div class="circle" onclick="onSubmit();">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 78.369 78.369">
+                    <path d="M78.05 19.015l-48.592 48.59c-.428.43-1.12.43-1.548 0L.32 40.016c-.427-.426-.427-1.12 0-1.547l6.704-6.704c.428-.427 1.12-.427 1.548 0l20.113 20.112 41.113-41.113c.43-.427 1.12-.427 1.548 0l6.703 6.704c.427.427.427 1.12 0 1.548z" fill="white"/>
+                </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    ${answers}	    
+                </div>
+                <div class="col-3"></div>
+            </div>
 		`
     };
     this.renderAnswer = function () {
         var result = ['', 0];
         for (key in this.professions) {
             if (this.professions[key] > result[1]) {
-                // console.log(key);
                 result[1] = this.professions[key];
                 result[0] = key;
             }
         }
-
         if (this.store['next']) {
             this.setStore(this.store['next'][result[0]]);
             this.refresh();
@@ -81,9 +100,24 @@ function Test(url, container) {
         }
         if (this.store['results']) {
             return `
-                <div>
-                    ${this.store['results'][result[0]]}
+                <div class="header post-header">
+                 <div class="header-wrapper">
+                    <div class="wrapper wrapper-left">
+                    </div>
+                    <div class="header-middle">
+                        <div class="title">${this.store['results'][result[0]]}</div>
+                    </div>
+                    <div class="wrapper wrapper-right-check">
+                    </div>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    ${this.store['results'][result[0]]}	    
+                </div>
+                <div class="col-3"></div>
+            </div>
             `
         }
 
