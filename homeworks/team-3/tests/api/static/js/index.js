46c93942c38842de0cb73bfad917a3a9cc8c81a2
matvey <hodovanyuk.mi@gmail.com>
2016-08-02 00:46:50 +0300

    changed init url

--- homeworks/team-3/tests/api/static/js/index.js
+++ homeworks/team-3/tests/api/static/js/index.js
@@ -2,6 +2,6 @@
 window.onload = initTesting;
 
 function initTesting(argument) {
-	var mainTest = Test('http://localhost:5000/api/v1/tests/1/questions','container');
+	var mainTest = Test('http://localhost:5000/api/v1/tests/1','container');
 	mainTest.init();
 }
